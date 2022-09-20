853e73d8f7b81112ef131c2d48795e2ca56e4b64
zoardev <info@zoar.dev>
2022-09-20 22:02:37 +0700

    remove comment code

--- src/libs/gtm.ts
+++ src/libs/gtm.ts
@@ -1,11 +1,7 @@
-export const gtmHeader = `<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+export const gtmHeader = `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P55TTH7');</script>
-<!-- End Google Tag Manager -->`;
-export const gtmBody = `<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P55TTH7"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->`
+})(window,document,'script','dataLayer','GTM-P55TTH7');</script>`;
+export const gtmBody = `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P55TTH7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`
